301dfcb54added8c14d744826db4ec80f226dd50
Haule9-2 <76088882+Haule9-2@users.noreply.github.com>
2023-04-18 00:14:43 +0700

    Add files via upload

---
+++ Game_Loto/PlayerInfo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Loto
+{
+    public class PlayerInfo
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+        private string money = "25000";
+        public string Money
+        {
+            get { return money; }
+            set { money = value; }
+        }
+
+
+        private string avt;
+        public string Avt        
+        {
+            get { return avt; }
+            set { avt = value; }
+        }
+        private string win;
+        public string Win
+        {
+            get { return win; }
+            set { win = value; }
+        }
+
+        
+        public PlayerInfo(string name,string money, string avata,string win)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.avt =avata;
+            this.win = win;
+            
+        }
+    }
+}
